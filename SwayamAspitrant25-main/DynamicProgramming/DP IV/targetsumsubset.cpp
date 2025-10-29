a1c7cb5c51a074d7609093054f7b88cbc982b93f
Sachinn-18 <sachinbst1234@gmail.com>
2025-10-29 15:36:25 +0530

    Min Path Sum

---
+++ SwayamAspitrant25-main/DynamicProgramming/DP IV/targetsumsubset.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if there is a subset with given sum using DP
+bool isSubsetSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+    for (int i = 0; i <= n; ++i) {
+        dp[i][0] = true;
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= target; ++j) {
+            if (j >= nums[i-1]) {
+                dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][target];
+}
+
+int main() {
+    vector<int> nums = {3, 34, 4, 12, 5, 2};
+    int target = 9;
+    if (isSubsetSum(nums, target)) {
+        cout << "Subset with sum " << target << " exists." << endl;
+    } else {
+        cout << "No subset with sum " << target << " exists." << endl;
+    }
+    return 0;
+}
